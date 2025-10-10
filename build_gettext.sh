177d4488198006fd92b4515895c5751432e99b5e
rmagiga <rharuakim-soft@yahoo.co.jp>
2025-10-10 22:31:49 +0900

    build_gettext.shのダウンロードURLを変数化し、抽出ディレクトリの指定を修正。configureオプションを整理。

--- build_gettext.sh
+++ build_gettext.sh
@@ -4,17 +4,29 @@ SCRIPTDIR=$(cd $(dirname $0); pwd)
 . $SCRIPTDIR/env.sh
 . $SCRIPTDIR/build_common.sh
 
+URL=https://ftpmirror.gnu.org/gettext/gettext-$GETTEXT_VERSION.tar.gz
 DOWNLOAD_FILE=${DOWNLOADDIR}/gettext-$GETTEXT_VERSION.tar.gz
+EXTRACT_DIR=$SRCDIR/gettext-$GETTEXT_VERSION
 
 # ダウンロード
-download https://ftpmirror.gnu.org/gettext/gettext-$GETTEXT_VERSION.tar.gz $DOWNLOAD_FILE
-extract $DOWNLOAD_FILE $SRCDIR/gettext-$GETTEXT_VERSION
+download $URL $DOWNLOAD_FILE
+extract $DOWNLOAD_FILE $EXTRACT_DIR
 
-cd $SRCDIR/gettext-$GETTEXT_VERSION
+cd $EXTRACT_DIR
 ./configure --host=$TARGET \
 	--prefix=$PREFIX \
 	--enable-static \
-	--enable-shared
+	--enable-shared \
+	--disable-java \
+	--disable-d \
+	--disable-modula2 \
+	--disable-c++ \
+	--disable-libasprintf \
+	--disable-curses \
+	--disable-openmp \
+	--disable-acl \
+	--disable-xattr \
+	--disable-examples
 
 make -j$(nproc)
 make install
